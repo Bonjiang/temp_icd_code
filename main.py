63f96f1261b6b8f040643c1e691d70bb26695f50
bonjiang <bonnie.jiang@stonybrook.edu>
2023-05-11 22:22:23 +0000

    hi theree

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, request 
+import pandas as pd 
+
+df = pd.read_csv('./data/diagnoses2019.csv')
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+def home():
+    return 'this is a API service for MN ICD code details'
+
+@app.route('/preview', methods=["GET"])
+def preview():
+    top10rows = df.head(1)
+    result = top10rows.to_json(orient="records")
+    return result
+
+@app.route('/countycode/<value>', methods=['GET'])
+def icdcode(value):
+    print('value: ', value)
+    filtered = df[df['county_code '] == value]
+    if len(filtered) <= 0:
+        return 'There is nothing here'
+    else: 
+        return filtered.to_json(orient="records")
+
+@app.route('/countycode/<value>/payer/<value2>')
+def icdcode2(value, value2):
+    filtered = df[df['county_code'] == value]
+    filtered2 = filtered[filtered['payer'] == value2]
+    if len(filtered2) <= 0:
+        return 'There is nothing here'
+    else: 
+        return filtered2.to_json(orient="records")    
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
