fbe51c7694d052a85034058c0383685c1eab2c80
bonjiang <bonnie.jiang@stonybrook.edu>
2023-05-11 22:33:18 +0000

    edit 2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request 
 import pandas as pd 
 
-df = pd.read_csv('./data/diagnoses2019.csv')
+df = pd.read_csv('./MNdataUtilization.csv')
 
 app = Flask(__name__)
 
@@ -15,18 +15,18 @@ def preview():
     result = top10rows.to_json(orient="records")
     return result
 
-@app.route('/countycode/<value>', methods=['GET'])
+@app.route('/county_name/<value>', methods=['GET'])
 def icdcode(value):
     print('value: ', value)
-    filtered = df[df['county_code '] == value]
+    filtered = df[df['county_name'] == value]
     if len(filtered) <= 0:
         return 'There is nothing here'
     else: 
         return filtered.to_json(orient="records")
 
-@app.route('/countycode/<value>/payer/<value2>')
+@app.route('/county_name/<value>/payer/<value2>')
 def icdcode2(value, value2):
-    filtered = df[df['county_code'] == value]
+    filtered = df[df['county_name'] == value]
     filtered2 = filtered[filtered['payer'] == value2]
     if len(filtered2) <= 0:
         return 'There is nothing here'
